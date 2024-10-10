8296e0bb9cd7bf308b52ec884eaac8201d1059ee
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2024-10-10 06:24:33 +0300

    Delete Logging.h

--- Logging.h
+++
@@ -1,6 +0,0 @@
-#ifndef MY_SERVICE_V1_00_LOGGING_H
-#define MY_SERVICE_V1_00_LOGGING_H
-
-void handle_error(const std::string &error_message); // Error handling function
-
-#endif // MY_SERVICE_V1_00_LOGGING_H
